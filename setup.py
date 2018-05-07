ace19996a2436061760007b665445a506521aea4
Thibault Cohen <titilambert@gmail.com>
2018-05-07 18:25:13 -0400

    Release 0.2.2

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import setup
 install_requires = list(val.strip() for val in open('requirements.txt'))
 tests_require = list(val.strip() for val in open('test_requirements.txt'))
 
+long_desc = open('README.rst').read()
+
 setup(name='pymycity',
-      version='0.2.1',
+      version='0.2.2',
       description='Get your city live information',
+      long_description=long_desc,
       author='Thibault Cohen',
+
       author_email='titilambert@gmail.com',
-      url='http://github.co,/titilambert/pymycity',
+      url='http://github.com/titilambert/pymycity',
       packages=['pymycity'],
       entry_points={
           'console_scripts': [
